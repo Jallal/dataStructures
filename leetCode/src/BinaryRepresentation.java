94f77ff444b7666265ee995d4575754e772352d1
Jallal ☁ <jhazzat@gmail.com>
2023-11-10 15:28:58 +0000

    [Jallal] added more solution to leetcode problemss

--- leetCode/src/BinaryRepresentation.java
+++ leetCode/src/BinaryRepresentation.java
@@ -1,12 +1,200 @@
+// import java.util.Map;
+
+// import javax.print.attribute.HashAttributeSet;
+import java.lang.*;
+import java.util.*;
+
 public class BinaryRepresentation {
 
     public static void main(String[] args) {
 
-        System.out.println(">>>>>>>> Binary Representation >>>>>>>>>>>> : " + Integer.toString(51, 2));
-        System.out.println(">>>>>>>> Binary Representation >>>>>>>>>>>> : " + Integer.toBinaryString(51));
+        // System.out.println("******************");
+        // int[] nums1 ={1,3};
+        // int[] nums2 = {2};
+        // List<Integer> list1 = new ArrayList<>();
+        // List<Integer> list2 = new ArrayList<>();
+        // list1.add(1);
+        // list1.add(2);
+        // list2.add(3);
+        //   list2.add(4);
 
-        //reverse from binaary to decimal
+        // System.out.println("Answer Array: "+findMedianSortedArrays(nums1,nums2));
 
-        System.out.println(">>>>>>>> Decimal Representation >>>>>>>>>>>> : " + Integer.parseInt("110011", 2));  //51
+        // System.out.println("Answer List: "+findMedianSortedLists(list1,list2));
+
+        // String s = "ac"; 
+        // System.out.println("Answer Array: "+ longestPalindrome(s));
+
+           System.out.println("Answer int: "+ reverse(123));
+           System.out.println("Answer int: "+ reverse(-123));
+           System.out.println("Answer int: "+ reverse(120));
+ 
     }
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int combinedSize = 0;
+        double median = 0.0;
+
+        if(nums1.length>0){
+
+            combinedSize = combinedSize + nums1.length;
+        }
+        if(nums2.length>0){
+
+            combinedSize = combinedSize+ nums2.length;
+        }
+
+        int[] combinedarr = new int[combinedSize];
+        System.out.println(" Combined array size : ****************** : "+combinedSize);
+        //System.arraycopy(source_arr, sourcePos, dest_arr, destPos, len);
+        System.arraycopy(nums1,0,combinedarr,0,nums1.length);
+        System.out.println(Arrays.toString(combinedarr));
+        System.arraycopy(nums2,0,combinedarr,nums1.length,nums2.length);
+        System.out.println(Arrays.toString(combinedarr));
+        Arrays.sort(combinedarr);
+        System.out.println(Arrays.toString(combinedarr));
+
+        if(combinedSize % 2 != 0 ){
+
+            median = (double)(combinedarr[combinedSize/2]);
+        } else{
+            
+            median = (double)(combinedarr[(combinedSize - 1) / 2] + combinedarr[combinedSize / 2]) / 2.0;
+        }
+
+        return median;
+    }
+
+
+
+     public static double findMedianSortedLists(List<Integer> nums1, List<Integer> nums2) {
+         int combinedSize = 0;
+        double median = 0.0;
+        nums1.addAll(nums2);
+        Collections.sort(nums1);
+        combinedSize = nums1.size();
+        System.out.println(" Combined array size : ****************** : "+combinedSize);
+        if(combinedSize % 2 != 0 ){
+
+            median = nums1.get(combinedSize/2);
+        } else{
+            
+            median = (nums1.get((combinedSize - 1) / 2) + nums1.get(combinedSize / 2)) / 2.0;
+        }
+
+        return median;
+    }
+
+
+//     Given a string s, return the longest 
+// palindromic
+ 
+// substring
+//  in s.
+
+ 
+
+// Example 1:
+
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+// Example 2:
+
+// Input: s = "cbbd"
+// Output: "bb"
+
+        /**
+         * @param s
+         * @return
+         */
+        public static String longestPalindrome(String s) {
+            char[] palindrome = new char[s.length()];
+            char[] arr = s.toCharArray();
+            String answer = null;
+          
+
+            for(int i = 0; i < arr.length ; i++){
+
+                if(arr[i]==arr[(arr.length-1) - i]){
+                    palindrome[i]=arr[i];
+                    palindrome[(arr.length-1)  - i]=arr[(arr.length-1)  - i];
+                }
+
+            }
+            StringBuilder finalString = new StringBuilder();
+
+            for(char b : palindrome){
+                if(!Character.isWhitespace(b)){
+                    finalString.append(b);
+
+                }
+
+            }
+    
+                
+            return finalString.toString().trim();
+        }
+
+
+
+        // Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+        // Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+            //Example 1:
+            // Input: x = 123
+            // Output: 321
+
+            // Example 2:
+            // Input: x = -123
+            // Output: -321
+
+            // Example 3:
+            // Input: x = 120
+            // Output: 21
+
+            
+        
+
+            public static int reverse(int x) {
+              
+                String str = Integer.toString(x);  
+                char[] cha = str.toCharArray();
+                boolean isNegativeDigit = false;
+                StringBuilder reveredInt = new StringBuilder();
+                if(cha.length !=0 && cha[0] == '-'){
+                    isNegativeDigit = true;
+                }
+
+                for(int i=0; i< cha.length ;i++ ){
+                //if(Character.isDigit(cha[(cha.length-1) -i])){
+                reveredInt.append(cha[(cha.length-1) -i]);
+              // }
+                }
+
+                int revertInt = Integer.valueOf(reveredInt.toString());
+
+                if(isNegativeDigit){
+
+                revertInt = revertInt*-1;
+                }
+
+                return revertInt;
+            }
+
+
+
+             public static boolean isPalindrome(int x) {
+                String str = Integer.toString(x);
+                char[] arr = str.toCharArray();
+                boolean answer = false;
+                for(int i = 0; i < arr.length ; i++){
+                    if(arr[i]==arr[(arr.length-1) - i]){
+                        answer = true;
+                    }else{
+                        return false;
+                    }
+                }
+                return answer;
+        }
+
+
 }
--- leetCode/src/UniqueCharacters.java
+++ leetCode/src/UniqueCharacters.java
@@ -1,6 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
-
 /***
  * Problem: Consider a string that can contain ASCII and Unicode characters ranging between 0-65,535.
  * Write a snippet of code that returns true if this string contains unique characters.
@@ -10,39 +7,126 @@ import java.util.Map;
  *
  * */
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+
 public class UniqueCharacters {
 
 
-    private static final int MAX_CODE = 65535;
+    // private static final int MAX_CODE = 65535;
 
 
-    public static boolean isUnique(String str) {
+    // public static boolean isUnique(String str) {
 
-        Map<Character, Boolean> chars = new HashMap<>();
+    //     Map<Character, Boolean> chars = new HashMap<>();
 
-        // or use, for(char ch : str.toCharArray()) { ... }
-        for (int i = 0; i < str.length(); i++) {
-            if (str.codePointAt(i) <= MAX_CODE) {
-                char ch = str.charAt(i);
-                if (!Character.isWhitespace(ch)) {
-                    if (chars.put(ch, true) != null) {
-                        return false;
-                    }
-                }
-            } else {
+    //     // or use, for(char ch : str.toCharArray()) { ... }
+    //     for (int i = 0; i < str.length(); i++) {
+    //         if (str.codePointAt(i) <= MAX_CODE) {
+    //             char ch = str.charAt(i);
+    //             if (!Character.isWhitespace(ch)) {
+    //                 if (chars.put(ch, true) != null) {
+    //                     return false;
+    //                 }
+    //             }
+    //         } else {
 
-                System.out.println("The given string contains unallowed characters");
-                return false;
-            }
+    //             System.out.println("The given string contains unallowed characters");
+    //             return false;
+    //         }
 
-        }
-        return true;
+    //     }
+    //     return true;
+    // }
+
+    // public static Map<String, String> getHighFrequencyItems(Map<String, String> allElement){
+
+    //     Integer target =null;
+    //     Integer frequency =null;
+    //     for(Map.entry<String, String> element : allElement.entrySet()){
+    //         if(element.getKey() >= target){
+    //             target = element.getKey();
+    //             frequency = element.getValue();
+    //         }else{
+    //             allElement.remove(element);
+    //         }
+    //     }
+
+    // }
+
+    // // public static Map<String, String> getElementWIthFrequency(List<Integer>  arr){
+     
+    // //  Map<Integer, Integer> allElement = new HashMap<>();
+
+    // //         for(Integer item : arr ){
+             
+    // //             if(!allElement.containsKey(item)){
+    // //                 allElement.put(item, 1);
+    // //             }else{
+    // //              allElement.put(item, allElement.get(item)+1);
+    // //             }
+    // //         }
+    // // }
+
+    // public static ap<Integer, Integer> getLongestSubArray(Map<Integer, Integer> highFrequency, List<String> arr){
+
+
+    //                Map<Integer, Integer> subArrMap = new HashMap<>();
+
+    //                 for(Map.Entry<Integer, Integer> item : highFrequency.entrySet()){
+
+    //                  subArrMap.put(arr.indexOf(item.toString),arr.lastIndexOf(item.toString));
+    //                 }
+
+
+
+    //                 for(Map.Entry<Integer, Integer> finalItem : subArrMap.entrySet()){
+
+    //                     int subArrLength = finalItem.getValue() - finalItem.getKey();
+
+                    
+
+    //                 }
+            
+    //                 return subArrMap;
+
+    // }
+
+       public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        double median=0.0;
+     
+        int[] combinedArr = new int[nums1.length + nums2.length];
+
+            System.arraycopy(nums2, 0, combinedArr, 0, combinedArr.length);
+
+
+
+       return median;
     }
 
 
     public static void main(String[] args) {
 
         String str = "Hello my name is Jallal";
-        System.out.println(">>>>>>>> is String unique >>>>>>>>>>>> : " + isUnique(str));
+        //System.out.println(">>>>>>>> is String unique >>>>>>>>>>>> : " + isUnique(str));
+
+        //List<Integer>  arr = new ArrayList<>();
+        //Map<Integer, Integer> allElement = getElementWIthFrequency(arr);
+       // Map<Integer, Integer> highFrequency = getHighFrequencyItems(allElement);
+        // Map<Integer, Integer> subArrMap = getLongestSubArray(highFrequency);
+
+
+        //find the median of two arries 
+        int[] nums1 = {1,4,8,9};
+        int[] nums2 = {7,2,6};
+
+        findMedianSortedArrays(nums1,nums2);
+
     }
 }
